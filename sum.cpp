ce735ef807bb6521d63318887380c5fd7f62b3d5
schats619 <sourav.c619@gmail.com>
2016-01-05 18:48:53 -0800

    sum.cpp file is added to find the sum of 2 numbers

---
+++ sum.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+	int a;
+		int b;
+			int sum;
+				
+					cout<<"Enter a value for a"<<endl
+						cin>>a;
+							cout<<"Enter a value for b"<<endl;
+								cin>>b;
+									
+										sum=a+b;
+											cout<<"The sum of a and b is :"<<sum;
+												
+													getch();
+														return 0;
+															
+															}
+
